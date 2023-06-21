626456aac579231bc25797cefbf8c5c8ac7cb6c1
Kasun-Abeywickrama <121012573+Kasun-Abeywickrama@users.noreply.github.com>
2023-06-21 09:18:17 +0530

    Updated practical05 - Q1

--- Lab07/Practical-05/Q1/Main/src/main/java/org/example/MyFirstInterface.java
+++ Lab07/Practical-05/Q1/Main/src/main/java/org/example/MyFirstInterface.java
@@ -5,16 +5,7 @@ public interface MyFirstInterface {
         int x = 10;
         //public static final int x = 10; //No error :- This method is also working properly.
 
-        <br>void display();
+        void display();
         //abstract void display(); //No error :- This method is also working properly.
 }
 
-/*public class MyFirstInterface {
-    int x = 10;
-
-    //void display(); // Error :- A normal class cannot keep abstract method(A method without body).
-    void display(){
-                  x = 20;
-                  System.out.println("The value of x is : "+x);
-    }
-}*/
